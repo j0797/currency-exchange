a386e3de466165cbf3f31bbbd94e1d36a58d4c7a
j0797 <anonymous@example.com>
2026-02-04 03:13:39 +0300

    feat: add Currency and ExchangeRate model classes

---
+++ src/main/java/com/currencyexchange/model/ExchangeRate.java
@@ -0,0 +1,51 @@
+package com.currencyexchange.model;
+
+import java.math.BigDecimal;
+
+public class ExchangeRate {
+    private Integer id;
+    private Currency baseCurrency;
+    private Currency targetCurrency;
+    private BigDecimal rate;
+
+    public ExchangeRate() {
+    }
+
+    public ExchangeRate(Currency baseCurrency, Currency targetCurrency, BigDecimal rate) {
+        this.baseCurrency = baseCurrency;
+        this.targetCurrency = targetCurrency;
+        this.rate = rate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Currency getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public void setBaseCurrency(Currency baseCurrency) {
+        this.baseCurrency = baseCurrency;
+    }
+
+    public Currency getTargetCurrency() {
+        return targetCurrency;
+    }
+
+    public void setTargetCurrency(Currency targetCurrency) {
+        this.targetCurrency = targetCurrency;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
+    }
+}
