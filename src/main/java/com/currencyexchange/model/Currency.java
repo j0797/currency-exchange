a68e245bd3dee045d1b234b39b1be60e960d4af0
j0797 <anonymous@example.com>
2026-02-10 23:45:36 +0300

    feat: add Currency and ExchangeRate model classes

---
+++ src/main/java/com/currencyexchange/model/Currency.java
@@ -0,0 +1,49 @@
+package com.currencyexchange.model;
+
+public class Currency {
+    private Integer id;
+    private String code;
+    private String fullName;
+    private String sign;
+    public Currency() {
+    }
+
+    public Currency(Integer id, String code, String fullName, String sign) {
+        this.id = id;
+        this.code = code;
+        this.fullName = fullName;
+        this.sign = sign;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String name) {
+        this.fullName = name;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+}
